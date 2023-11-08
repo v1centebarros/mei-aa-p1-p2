d78fdc07d0fe025f269ac63eda445aa5873a637a
v1centebarros <vmabarros@ua.pt>
2023-11-08 17:00:33 +0000

    Identation

--- src/algorithms.py
+++ src/algorithms.py
@@ -4,57 +4,67 @@ from utils import benchmark
 """
     This function is slower than the one below
 """
+
+
 # def is_vertex_cover(graph, vertices):
 #     return all([u in vertices or v in vertices for u, v in graph.edges()])
 
 def is_vertex_cover(graph, vertices):
+    operation_count = 0  # Initialize operation counter
     for u, v in graph.edges():
+        operation_count += 1
         if u not in vertices and v not in vertices:
-            return False
-    return True
+            operation_count += 1
+            return False, operation_count  # Return operation count
+    return True, operation_count  # Return operation count
+
 
 @benchmark
 def bruteforce(graph):
-   cover = set()
-   operation_count = 0 # Initialize operation counter
+    cover = set()
+    operation_count = 0  # Initialize operation counter
 
-   # Iterate over all possible subsets of vertices
-   for i in range(1, len(graph.nodes()) + 1):
-       for vertices in combinations(graph.nodes(), i):
-           operation_count += 1 # Increment operation counter for each combination
-           if is_vertex_cover(graph, vertices):
-               operation_count += 1 # Increment operation counter for each check
-               if len(cover) == 0 or len(vertices) < len(cover):
-                  cover = set(vertices)
+    # Iterate over all possible subsets of vertices
+    for i in range(1, len(graph.nodes()) + 1):
+        for vertices in combinations(graph.nodes(), i):
+            operation_count += 1  # Increment operation counter for each combination
+            is_cover, oc = is_vertex_cover(graph, vertices)
+            operation_count += oc
+            if is_cover:
+                operation_count += 1  # Increment operation counter for each check
+                if len(cover) == 0 or len(vertices) < len(cover):
+                    cover = set(vertices)
 
-   return cover, operation_count # Return operation count
+    return cover, operation_count  # Return operation count
 
 
 @benchmark
 def bruteforce_bitwise(graph):
-   cover = set()
-   operation_count = 0 # Initialize operation counter
+    """
+        :deprecated: This function is deprecated because it is slower than bruteforce
+    """
+    cover = set()
+    operation_count = 0  # Initialize operation counter
 
-   # Iterate over all possible subsets of vertices
-   for i in range(1, 2**len(graph.nodes())):
-       vertices = set()
-       for j in range(len(graph.nodes())):
-           operation_count += 1 # Increment operation counter for each bitwise operation
-           if i & (1 << j):
-               vertices.add(j)
-       if is_vertex_cover(graph, vertices):
-           operation_count += 1 # Increment operation counter for each check
-           if len(cover) == 0 or len(vertices) < len(cover):
-               cover = set(vertices)
+    # Iterate over all possible subsets of vertices
+    for i in range(1, 2 ** len(graph.nodes())):
+        vertices = set()
+        for j in range(len(graph.nodes())):
+            operation_count += 1  # Increment operation counter for each bitwise operation
+            if i & (1 << j):
+                vertices.add(j)
+        if is_vertex_cover(graph, vertices):
+            operation_count += 1  # Increment operation counter for each check
+            if len(cover) == 0 or len(vertices) < len(cover):
+                cover = set(vertices)
 
-   return cover, operation_count # Return operation count
-
+    return cover, operation_count  # Return operation count
 
 
 @benchmark
 def greedy_vertex_cover(graph):
     cover = set()
-    operation_count = 0 # Initialize operation counter
+    operation_count = 0  # Initialize operation counter
 
     # As long as there are edges in the graph
     while graph.number_of_edges() > 0:
@@ -68,4 +78,4 @@ def greedy_vertex_cover(graph):
         graph.remove_node(v)
         operation_count += 2  # Increment operation counter for each addition and removal
 
-    return cover, operation_count # Return operation count
+    return cover, operation_count  # Return operation count
